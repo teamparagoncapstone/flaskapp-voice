4e1e2a6d4c8039d28865dc0828a48aec1b6295e6
teamparagoncapstone <167285778+teamparagoncapstone@users.noreply.github.com>
2024-11-05 14:02:17 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, jsonify, request
-from models import db, VoiceExcercises, VoiceExcercisesHistory, Module
+from models import db, VoiceExcercises, VoiceExcercisesHistory, Module, Award
+from datetime import datetime
 import os
 from dotenv import load_dotenv
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 import speech_recognition as sr
 from pydub import AudioSegment
 from transformers import BertTokenizer, BertModel, RobertaTokenizer, RobertaModel
@@ -28,58 +29,163 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 migrate.init_app(app, db) 
 
-CORS(app, resources={r"/api/*": {"origins": "https://capstone-lms-red.vercel.app"}})
+CORS(app, origins=[
+    "http://localhost:3000", 
+    "https://capstone-lms.vercel.app", 
+    "https://capstone-b5lfpvbba-paragons-projects-6f921143.vercel.app",
+    "https://capstone-2cte19f7l-paragons-projects-6f921143.vercel.app"  
+]) 
+
+@app.route('/', methods=['GET'])
+def hello_world():
+    return jsonify({"message": "Hello World!"}), 200
+
+def assign_award(average_score):
+    """Helper function to assign award based on average score"""
+    if average_score >= 95:
+        return "Star Badge"
+    elif average_score >= 90:
+        return "Gold Badge"
+    elif average_score >= 80:
+        return "Silver Badge"
+    elif average_score >= 70:
+        return "Bronze Badge"
+    return None
+
+def calculate_student_voice_awards(student_id):
+    """Calculate student awards based on voice exercise history"""
+  
+    voice_history = VoiceExcercisesHistory.query.filter_by(studentId=student_id).all()
+
+  
+    if not voice_history:
+        return jsonify({"message": "No voice exercise history found for this student."}), 200
+
+   
+    total_voice_score = sum([voice.score for voice in voice_history])
+    average_voice_score = total_voice_score / len(voice_history) if voice_history else 0
+
+ 
+    voice_award = assign_award(average_voice_score)
+
+    
+    if voice_award:
+        new_award = Award(
+            studentId=student_id,
+            awardType=voice_award,
+            tier="Voice Exercises",
+            createdAt=datetime.now()
+        )
+        db.session.add(new_award)
+        db.session.commit()
+
+    return jsonify({"message": "Voice awards calculated and saved."}), 200 
 
 @app.route('/api/voice-exercises', methods=['GET'])
+@cross_origin(origins=["http://localhost:3000", "https://capstone-lms.vercel.app", "https://capstone-b5lfpvbba-paragons-projects-6f921143.vercel.app"])
 def get_voice_exercises():
     try:
-        # Get the module title from query parameters
+        student_id = request.args.get('studentId')
         module_title = request.args.get('moduleTitle')
 
-        if module_title:
-            # Filter by module title if provided
-            exercises = VoiceExcercises.query.join(Module).filter(Module.moduleTitle == module_title).all()
-        else:
-            # Query all records if no module title is provided
-            exercises = VoiceExcercises.query.all()
+        if not student_id or not module_title:
+            return jsonify({'error': 'Student ID and Module title are required'}), 400
 
-        results = [
-            {
+        exercises = VoiceExcercises.query.join(Module).filter(Module.moduleTitle == module_title).all()
+        
+        results = []
+        for ex in exercises:
+           
+            completed_exercise = VoiceExcercisesHistory.query.filter_by(
+                studentId=student_id, voiceExercisesId=ex.id, completed=True
+            ).first()
+
+            exercise_data = {
                 'id': ex.id,
                 'voice': ex.voice,
                 'grade': ex.grade,
                 'voiceImage': ex.voiceImage,
-                'userId': ex.userId,
-                'createdAt': ex.createdAt,
-                'updatedAt': ex.updatedAt,
+                'isCompleted': completed_exercise is not None,
+               
+                'scores': {
+                    'accuracy_score': completed_exercise.accuracyScore,
+                    'pronunciation_score': completed_exercise.pronunciationScore,
+                    'fluency_score': completed_exercise.fluencyScore,
+                    'speed_score': completed_exercise.speedScore,
+                    'final_score': completed_exercise.score,
+                    'grade': completed_exercise.grade,
+                } if completed_exercise else None
             }
-            for ex in exercises
-        ]
-        return jsonify(results), 200
+            results.append(exercise_data)
 
+        return jsonify({
+            'status': 'success',
+            'exercises': results
+        }), 200
     except Exception as e:
         print(f"Error fetching voice exercises: {e}")
         return jsonify({'error': 'An error occurred while fetching data.'}), 500
 
+def assign_award(average_score):
+    """Helper function to assign award based on average score"""
+    if average_score >= 95:
+        return "Star Badge"
+    elif average_score >= 90:
+        return "Gold Badge"
+    elif average_score >= 80:
+        return "Silver Badge"
+    elif average_score >= 70:
+        return "Bronze Badge"
+    return None
+
+def calculate_student_voice_awards(student_id):
+    """Calculate student awards based on voice exercise history"""
+  
+    voice_history = VoiceExcercisesHistory.query.filter_by(studentId=student_id).all()
+
+    
+    if not voice_history:
+        return jsonify({"message": "No voice exercise history found for this student."}), 200
+
+    # Calculate total and average scores for voice exercises
+    total_voice_score = sum([voice.score for voice in voice_history])
+    average_voice_score = total_voice_score / len(voice_history) if voice_history else 0
+
+  
+    voice_award = assign_award(average_voice_score)
+
+    
+    if voice_award:
+        new_award = Award(
+          studentId=student_id, 
+          awardType=voice_award, 
+          tier="Voice Exercises",
+          createdAt=datetime.now() 
+        )
+        db.session.add(new_award)
+        db.session.commit()
+
+    return jsonify({"message": "Voice awards calculated and saved."}), 200
+        
 sentence_transformer_model = SentenceTransformer('all-MiniLM-L6-v2')
 
-# Initialize BERT and RoBERTa models and tokenizers
+
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 bert_model = BertModel.from_pretrained('bert-base-uncased')
 
 roberta_tokenizer = RobertaTokenizer.from_pretrained('roberta-base')
 roberta_model = RobertaModel.from_pretrained('roberta-base')
 
-# Initialize NLTK CMU Pronouncing Dictionary
+
 nltk.download('cmudict')
 phoneme_dict = cmudict.dict()
 
-# Define Fluency Model
+
 class FluencyModel(nn.Module):
     def __init__(self):
         super(FluencyModel, self).__init__()
-        self.fc = nn.Linear(768 * 2, 1)  # Concatenate two BERT embeddings (768*2 dimensions)
-        self.sigmoid = nn.Sigmoid()  # Use sigmoid to scale output between 0 and 1
+        self.fc = nn.Linear(768 * 2, 1) 
+        self.sigmoid = nn.Sigmoid()  #
     
     def forward(self, x):
         return self.sigmoid(self.fc(x))
@@ -113,13 +219,13 @@ def calculate_advanced_similarity(text1, text2):
     
     cosine_sim = torch.nn.functional.cosine_similarity(embedding1, embedding2, dim=0)
     
-    return cosine_sim.item() * 100  # Convert to percentage
+    return cosine_sim.item() * 100  
 
 def calculate_accuracy(expected_text, recognized_text):
     if not expected_text or not recognized_text:
         return 0.0
 
-    wer_score = wer(expected_text, recognized_text) * 100  # Convert to percentage
+    wer_score = wer(expected_text, recognized_text) * 100
     semantic_similarity = calculate_advanced_similarity(expected_text, recognized_text)
 
     combined_accuracy = (100 - wer_score + semantic_similarity) / 2
@@ -136,7 +242,7 @@ def calculate_semantic_similarity(text1, text2):
     embedding1 = outputs1.last_hidden_state.mean(dim=1).squeeze()
     embedding2 = outputs2.last_hidden_state.mean(dim=1).squeeze()
     cosine_sim = torch.nn.functional.cosine_similarity(embedding1, embedding2, dim=0)
-    return cosine_sim.item() * 100  # Convert to percentage
+    return cosine_sim.item() * 100  
 
 def calculate_fluency_score(expected_text, recognized_text):
     if not recognized_text or not expected_text:
@@ -156,7 +262,7 @@ def calculate_fluency_score(expected_text, recognized_text):
 
 def calculate_speed_score(audio_file, recognized_text):
     audio = AudioSegment.from_file(audio_file)
-    duration_seconds = len(audio) / 1000  # seconds
+    duration_seconds = len(audio) / 1000  
     num_words = len(recognized_text.split())
 
     if duration_seconds > 0:
@@ -164,7 +270,7 @@ def calculate_speed_score(audio_file, recognized_text):
     else:
         wps = 0
 
-    # Speed score based on WPM
+  
     if wps < 0.01:
         return 0
     elif 0.01 <= wps < 0.02:
@@ -247,12 +353,13 @@ def create_voice_exercise_history():
             audio_filename = 'recorded_audio.webm'
             audio_path = os.path.join(app.root_path, 'static', audio_filename)
 
-            # Save uploaded file
+           
             audio_blob.save(audio_path)
 
-            # Convert WebM to WAV using PyDub
+           
             audio = AudioSegment.from_file(audio_path, format="webm")
-            wav_path = os.path.join(app.root_path, 'static', 'recorded_audio.wav')
+            wav_filename = 'recorded_audio.wav'
+            wav_path = os.path.join(app.root_path, 'static', wav_filename)
             audio.export(wav_path, format="wav")
 
             try:
@@ -262,7 +369,6 @@ def create_voice_exercise_history():
             except sr.RequestError:
                 return jsonify({'error': 'Could not process audio; please try again'})
 
-            # Calculate scores
             accuracy_score = calculate_accuracy(expected_text, recognized_text)
             pronunciation_score = calculate_semantic_similarity(expected_text, recognized_text)
             fluency_score = calculate_fluency_score(expected_text, recognized_text)
@@ -272,42 +378,13 @@ def create_voice_exercise_history():
             final_score = round(final_score, 2)
             grade = get_grade(final_score)
 
-            phonemes = json.dumps(get_phonemes(recognized_text)) 
-            voice_image = request.form.get('voice_image')
-            voice_exercises_id = request.form.get('voice_exercises_id')
-            student_id = request.form.get('student_id')
+            phonemes = get_phonemes(recognized_text)
 
-            if not student_id or not voice_exercises_id:
-                return jsonify({'error': 'student_id and voice_exercises_id are required'}), 400
-            
+        
             with open(wav_path, "rb") as audio_file:
                 audio_base64 = base64.b64encode(audio_file.read()).decode('utf-8')
 
-            # Save the data to the database
-            new_history = VoiceExcercisesHistory(
-                voice=expected_text,
-                voiceRecord=audio_base64,
-                voiceImage=voice_image,
-                recognizedText=recognized_text,
-                accuracyScore=str(accuracy_score),
-                pronunciationScore=str(pronunciation_score),
-                fluencyScore=str(fluency_score),
-                speedScore=str(speed_score),
-                phonemes=phonemes,  
-                voiceExercisesId=voice_exercises_id, 
-                studentId=student_id,
-                score=final_score   
-            )
-            print(f"Inserting: {new_history}")
-
-            db.session.add(new_history)
-            try:
-                db.session.commit()
-            except Exception as e:
-                db.session.rollback() 
-                app.logger.error(f"Database insertion error: {e}")
-                return jsonify({'error': 'Failed to save to database'}), 500
-
+           
             return jsonify({
                 'accuracy_score': accuracy_score,
                 'pronunciation_score': pronunciation_score,
@@ -316,15 +393,84 @@ def create_voice_exercise_history():
                 'final_score': final_score,
                 'grade': grade,
                 'recognized_text': recognized_text,
-                'phonemes': phonemes
-                
+                'phonemes': phonemes,
+                'voiceRecord': audio_base64,
+                 
             }), 200  
         else:
             return jsonify({'error': 'No audio file or expected text received'}), 400
     except Exception as e:
-        app.logger.error(f"Error creating voice exercise history: {e}")
-        return jsonify({'error': 'An error occurred while creating history.'}), 500
+        app.logger.error(f"Error processing audio: {e}")
+        return jsonify({'error': 'An error occurred while processing audio.'}), 500
 
+
+@app.route('/api/submit-exercise', methods=['POST'])
+@cross_origin(origins=["http://localhost:3000", "https://capstone-lms.vercel.app"])
+def submit_exercise():
+    try:
+        data = request.get_json()
+        student_id = data.get('student_id')
+        voice_exercises_id = data.get('voice_exercises_id')
+
+       
+        if not student_id or not voice_exercises_id:
+            return jsonify({'error': 'Missing student ID or exercise ID'}), 400
+
+       
+        existing_history = VoiceExcercisesHistory.query.filter_by(
+            studentId=student_id, voiceExercisesId=voice_exercises_id, completed=True
+        ).first()
+
+        if existing_history:
+            return jsonify({'error': 'This exercise has already been completed.'}), 400
+
+     
+        expected_text = data.get('expected_text')
+        voice_image = data.get('voice_image')
+        recognized_text = data.get('recognized_text')
+        accuracy_score = data.get('accuracy_score')
+        pronunciation_score = data.get('pronunciation_score')
+        fluency_score = data.get('fluency_score')
+        speed_score = data.get('speed_score')
+        phonemes = data.get('phonemes')
+        final_score = data.get('final_score')
+        voice_record = data.get('voiceRecord')
+        grade = get_grade(final_score)
+       
+        new_history = VoiceExcercisesHistory(
+            voice=expected_text,
+            voiceRecord=voice_record,
+            voiceImage=voice_image,
+            recognizedText=recognized_text,
+            accuracyScore=accuracy_score,
+            pronunciationScore=pronunciation_score,
+            fluencyScore=fluency_score,
+            speedScore=speed_score,
+            phonemes=json.dumps(phonemes),
+            voiceExercisesId=voice_exercises_id,
+            studentId=student_id,
+            score=final_score,
+            completed=True,
+            grade=grade
+        )
+
+       
+        exercise = VoiceExcercises.query.filter_by(id=voice_exercises_id).first()
+        if exercise:
+            exercise.completed = True
+
+    
+        db.session.add(new_history)
+        db.session.commit()
+
+        calculate_student_voice_awards(student_id)
+
+        # Return success response
+        return jsonify({'success': True}), 200
+
+    except Exception as e:
+        logging.error(f"Error in submit_exercise: {str(e)}")
+        return jsonify({'error': 'Failed to submit exercise'}), 500
 
 
 if __name__ == '__main__':
